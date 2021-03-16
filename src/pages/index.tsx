342fca8b6f496610d0615c7554d4427ae4963997
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-16 14:10:54 -0300

    Title dinamico por pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head';
+
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
+    </>
   )
 }
