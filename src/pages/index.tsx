8732c40a84c796f21b8830542d447388d02e97ef
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-16 15:27:30 -0300

    Componente: SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubscribeButton';
+
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -17,6 +19,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
