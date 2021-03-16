7597b2f108f4e4e20b0f66aa85fff0ba72eb7d3f
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-16 15:08:59 -0300

    Componente: SignInButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -9,6 +11,8 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   );
