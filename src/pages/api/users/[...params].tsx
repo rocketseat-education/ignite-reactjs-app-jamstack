438bec81cda0132c6a389f88069d535a614045ee
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-17 14:46:42 -0300

    Parametrizacao nas rotas

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query)
+    
+  const users = [
+    { id: 1, name: 'Diego' },
+    { id: 2, name: 'Dani' },
+    { id: 3, name: 'Rafa' },
+  ]
+
+  return response.json(users)
+}
