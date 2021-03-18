900bacec22beae48690ecd7b74be7d1ad5b3d811
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-18 17:21:53 -0300

    Salvando dados do evento

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,71 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { Readable } from 'stream'
+import Stripe from "stripe";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('evento recebido')
+import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
-  res.status(200).json({ ok: true })
+async function buffer(readable: Readable) {
+  const chunks = [];
+  
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const buf = await buffer(req)
+    const secret = req.headers['stripe-signature']
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+      return res.status(400).send(`Webhook error: ${err.message}`);
+    }
+
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case 'checkout.session.completed':
+
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            )
+
+            break;
+          default:
+            throw new Error('Unhandled event.')
+        }
+      } catch (err) {
+        return res.json({ error: 'Webhook handler failed.' })
+      }
+    }
+
+    res.json({ received: true })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method not allowed')
+  }
 }
