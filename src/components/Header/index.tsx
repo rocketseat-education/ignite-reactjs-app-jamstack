333a24d2840313eaf30ddf18c66213193aa1f0e8
Diego Fernandes <diego.schell.f@gmail.com>
2021-03-19 13:58:18 -0300

    Navegacao no Menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
@@ -8,8 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
